f3548e7e1865cbe7cf26fa33fed5edd2ea124e16
Leedonguk <127273201+donguk1@users.noreply.github.com>
2023-06-01 09:33:21 +0900

    Add files via upload

---
+++ 11-10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+	FILE *wfp;
+	wfp = fopen("c:\\c_study\\gugu.txt", "w");
+
+	int i, k;
+
+	for (i = 2; i <= 9; i++) {
+		fprintf(wfp, "#Á¦%d´Ü#\t\t", i);
+	}
+	fprintf(wfp, "\n\n");
+
+	for (i = 1; i <= 9; i++) {
+		for ( k = 2; k <= 9; k++) {
+			fprintf(wfp, "%d X %d = %2d\t", k, i, k*i);
+		}
+		fprintf(wfp, "\n");
+	}
+	fclose(wfp);
+}
